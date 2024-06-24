87eeea736d048450e45315e88c0d8098376738bb
binod132 <49872458+binod132@users.noreply.github.com>
2024-06-24 17:24:45 +0545

    Update k6.js
    fix

--- k6.js
+++ k6.js
@@ -1,32 +1,23 @@
-import http from 'k6/http';
-import { sleep, check } from 'k6';
+import grpc from 'k6/net/grpc';
+import { check, sleep } from 'k6';
 
-export let options = {
-  stages: [
-    { duration: '30s', target: 10 },   // Ramp up to 10 virtual users over 30 seconds
-    { duration: '1m', target: 50 },    // Maintain 50 virtual users for 1 minute
-    { duration: '30s', target: 0 },    // Ramp down to 0 virtual users over 30 seconds
-  ],
-};
+const client = new grpc.Client();
+client.load(['definitions'], 'hello.proto');
 
-export default function () {
-  let url = 'http://dev-objectdetectionservice.default.svc.cluster.local:9080';
-
-  let params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  let response = http.get(url, params);
-
-  // Check response status
-  check(response, {
-    'is status 200': (r) => r.status === 200,
+export default () => {
+  client.connect('dev-objectdetectionservice.default.svc.cluster.local:9080', {
+    // plaintext: false
   });
 
-  // Optionally log response body
-  console.log(`Response body: ${response.body}`);
+  const data = { greeting: 'Bert' };
+  const response = client.invoke('hello.HelloService/SayHello', data);
 
+  check(response, {
+    'status is OK': (r) => r && r.status === grpc.StatusOK,
+  });
+
+  console.log(JSON.stringify(response.message));
+
+  client.close();
   sleep(1);
-}
+};
