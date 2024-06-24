a04296e4e195eec7869f4e23ef3985a630e6bbbb
binod132 <49872458+binod132@users.noreply.github.com>
2024-06-24 17:11:16 +0545

    Update k6.js
    fix

--- k6.js
+++ k6.js
@@ -7,7 +7,7 @@ export let options = {
 };
 
 export default function () {
-  let url = 'http://dev-objectdetectionservice.default.svc.cluster.local';
+  let url = 'http://dev-objectdetectionservice.default.svc.cluster.local:9080';
   let response = http.get(url);
   console.log(`Response status code: ${response.status}`);
   sleep(1);
