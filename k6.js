6fdcf5b237dee73bebb5e74d82ed82699414c026
binod132 <49872458+binod132@users.noreply.github.com>
2024-06-25 10:13:01 +0545

    Update k6.js
    fix

--- k6.js
+++ k6.js
@@ -1,8 +1,15 @@
 import grpc from 'k6/net/grpc';
 import { check, sleep } from 'k6';
+import { readFileSync } from 'fs';
+import path from 'path';
+
+const protoPath = path.resolve(__dirname, 'hello.proto');
+
+console.log('Current working directory:', __dirname);
+console.log('Proto file path:', protoPath);
 
 const client = new grpc.Client();
-client.load(['.'], 'hello.proto');
+client.load([protoPath]);
 
 export default () => {
   client.connect('dev-objectdetectionservice.default.svc.cluster.local:9080', {
